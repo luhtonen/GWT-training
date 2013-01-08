62cad86b7fd02e2810da076c1ef3061a4a7ad0cc
Eduard Luhtonen <luhtonen@gmail.com>
2013-01-08 18:29:13 +0200

    init version of basicproject

---
+++ edu-gwtia-ch03-basicapplication/src/org/elu/gwitia/ch03/basicapp/client/BasicProject.java
@@ -0,0 +1,11 @@
+package org.elu.gwitia.ch03.basicapp.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class BasicProject implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO
+	}
+}
